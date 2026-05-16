c426b098d56ed4bb055ac19ae5fa95eabf408460
sarva1210 <sarva.vish77@gmail.com>
2026-05-16 18:16:55 +0530

    pages

---
+++ frontend/src/pages/Desktop.jsx
@@ -0,0 +1,9 @@
+function Desktop() {
+  return (
+    <div className="h-screen bg-blue-900 text-white flex items-center justify-center text-5xl">
+      GOV.EXE DESKTOP
+    </div>
+  )
+}
+
+export default Desktop
---
+++ frontend/src/pages/Result.jsx
@@ -0,0 +1,9 @@
+function Result() {
+  return (
+    <div className="h-screen bg-red-900 text-white flex items-center justify-center text-4xl">
+      APPLICATION REJECTED SUCCESSFULLY
+    </div>
+  )
+}
+
+export default Result
