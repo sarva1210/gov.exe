c549e55065fea381e98b6ce4b90afb73c53cc876
sarva1210 <sarva.vish77@gmail.com>
2026-05-18 22:32:45 +0530

    chaosss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,10 @@
 import { BrowserRouter } from "react-router-dom"
-import { useEffect } from "react"
-import Cursor from "./components/Cursor"
 import AppRoutes from "./routes/AppRoutes"
-import { cursorChaos } from "./effects/cursorChaos"
 
 function App() {
-  useEffect(() => {
-    cursorChaos()
-  },[])
-
   return (
     <BrowserRouter>
-
-      <Cursor />
-
       <AppRoutes />
-
     </BrowserRouter>
   )
 }
